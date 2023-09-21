9647944670e11ffb1f8209a95b3d9160221be5f3
clabby <ben@clab.by>
2023-09-21 17:36:28 -0400

    ✨ CLI Flags

--- bin/src/cannon.rs
+++ bin/src/cannon.rs
@@ -2,23 +2,76 @@ use anyhow::{anyhow, Result};
 use clap::{ArgAction, Parser};
 use tracing::Level;
 
-/// A simple clap boilerplate
+/// Command line arguments for `cannon run`
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
 struct Args {
     /// Verbosity level (0-4)
-    #[arg(long, short, help = "Verbosity level (0-4)", action = ArgAction::Count)]
+    #[arg(long, short, action = ArgAction::Count, default_value = "2")]
     v: u8,
+
+    /// The path to the input JSON state.
+    #[arg(long)]
+    input: String,
+
+    /// The path to the output JSON state.
+    #[arg(long)]
+    output: Option<String>,
+
+    /// The step to generate an output proof at.
+    #[arg(long, short)]
+    proof_at: Option<u64>,
+
+    /// Format for proof data output file names. Proof data is written to stdout
+    /// if this is not specified.
+    #[arg(long)]
+    proof_format: Option<String>,
+
+    /// The step pattern to generate state snapshots at.
+    #[arg(long, short)]
+    snapshot_at: Option<String>,
+
+    /// Format for snapshot data output file names.
+    #[arg(long)]
+    snapshot_format: Option<String>,
+
+    /// The instruction step to stop running at.
+    #[arg(long)]
+    stop_at: Option<u64>,
+
+    /// The pattern to print information at.
+    #[arg(long, short)]
+    info_at: Option<String>,
+
+    /// An L1 RPC endpoint
+    #[arg(long, aliases = ["le"])]
+    l1_endpoint: String,
+
+    /// An L2 RPC endpoint
+    #[arg(long, aliases = ["la"])]
+    l2_endpoint: String,
 }
 
 fn main() -> Result<()> {
     // Parse the command arguments
-    let Args { v } = Args::parse();
+    let Args {
+        v,
+        l1_endpoint: _,
+        l2_endpoint: _,
+        input: _,
+        output: _,
+        proof_at: _,
+        proof_format: _,
+        snapshot_at: _,
+        snapshot_format: _,
+        stop_at: _,
+        info_at: _,
+    } = Args::parse();
 
     // Initialize the tracing subscriber
     init_tracing_subscriber(v)?;
 
-    tracing::debug!(target: "cannon-cli", "TODO");
+    tracing::info!(target: "cannon-cli", "TODO");
 
     Ok(())
 }
